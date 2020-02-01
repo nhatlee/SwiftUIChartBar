81a1b2339a5611694c6980f5f45b55c65e56e653
nhatlt2 <nhatlt2@vng.com.vn>
2020-02-01 11:12:04 +0700

    add overlay example

--- SwiftUIBarChart/ContentView.swift
+++ SwiftUIBarChart/ContentView.swift
@@ -8,44 +8,99 @@
 
 import SwiftUI
 
-// Test base components
+/*
+ Added overlay layer
+ */
 
 struct ContentView: View {
+    @State var showOverlay = false
+    @State var curColor = Color.blue
+    
     var body: some View {
-        VStack {
-            Rectangle()
-                .fill(Color.black)
-                .frame(width: 200, height: 200)
-
-            RoundedRectangle(cornerRadius: 25, style: .continuous)
-                .fill(Color.red)
-                .frame(width: 200, height: 200)
-
-            Capsule()
-                .fill(Color.green)
-                .frame(width: 100, height: 50)
-
-            Ellipse()
-                .fill(Color.blue)
-                .frame(width: 100, height: 50)
-
-            Circle()
-                .fill(Color.yellow)
-                .frame(width: 100, height: 50)
-            
-            Button(action: {
-                print("Button tapped")
-            }) {
-                Image(systemName: "bolt.fill")
-                    .foregroundColor(.white)
-                    .padding(EdgeInsets(top: 10, leading: 20, bottom: 10, trailing: 20))
-                    .background(Color.green)
-                    .clipShape(Capsule())
-            }
-        }
+        Text("Tap me to Show/Hide overlay")
+            .multilineTextAlignment(.center)
+            .frame(width: 100, height: 100)
+            .background(curColor)
+            .cornerRadius(20)
+            .onTapGesture { self.showOverlay.toggle() }
+            .overlay( ArcSelectionView(isShowing: self.$showOverlay, curColor: self.$curColor) )
     }
 }
 
+struct ArcSelectionView: View {
+    @Binding var isShowing : Bool
+    @Binding var curColor : Color
+    
+    let colors = [Color.blue, Color.red, Color.green, Color.yellow]
+    
+    var body: some View {
+        ZStack {
+            ForEach(1 ..< 5, id: \.self) { item in
+                Circle()
+                    .trim(from: self.isShowing ? CGFloat((Double(item) * 0.25) - 0.25) : CGFloat(Double(item) * 0.25),
+                          to: CGFloat(Double(item) * 0.25))
+                    .stroke(self.colors[item - 1], lineWidth: 30)
+                    .frame(width: 300, height: 300)
+                    .animation(.linear(duration: 0.4))
+                    .onTapGesture {
+                        self.curColor = self.colors[item - 1]
+                        self.isShowing.toggle()
+                }
+            }
+        }
+        //.opacity(self.isShowing ? 1 : 0)
+        //.rotationEffect(.degrees(self.isShowing ? 0 : 180))
+        //.animation(.linear(duration: 0.5))
+    }
+}
+
+
+
+
+/*
+Some base components
+ */
+
+//struct ContentView: View {
+//    var body: some View {
+//        VStack {
+//            Rectangle()
+//                .fill(Color.black)
+//                .frame(width: 200, height: 200)
+//
+//            RoundedRectangle(cornerRadius: 25, style: .continuous)
+//                .fill(Color.red)
+//                .frame(width: 200, height: 200)
+//
+//            Capsule()
+//                .fill(Color.green)
+//                .frame(width: 100, height: 50)
+//
+//            Ellipse()
+//                .fill(Color.blue)
+//                .frame(width: 100, height: 50)
+//
+//            Circle()
+//                .fill(Color.yellow)
+//                .frame(width: 100, height: 50)
+//
+//            Button(action: {
+//                print("Button tapped")
+//            }) {
+//                Image(systemName: "bolt.fill")
+//                    .foregroundColor(.white)
+//                    .padding(EdgeInsets(top: 10, leading: 20, bottom: 10, trailing: 20))
+//                    .background(Color.green)
+//                    .clipShape(Capsule())
+//            }
+//        }
+//    }
+//}
+
+
+/*
+ Chart view
+ */
 //struct ContentView: View {
 //    @State private var pickerSelectedItem = 0
 //    @State private var dataPoints: [[CGFloat]] = [
